eecf341dc462c80eb098f106b580689c4342a8a7
ericpkatz <ericpkatz@gmail.com>
2020-06-17 07:26:02 -0400

    add props for facets

--- client/components/Products.js
+++ client/components/Products.js
@@ -2,8 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
 
-const Products = ({products, filter, history, filtered}) => {
-  console.log(filter);
+const Products = ({products, filter, history, filtered, ratingFacet, stockFacet}) => {
   const search = (ev)=> {
 
     const _filter = {...filter };
@@ -29,15 +28,44 @@ const Products = ({products, filter, history, filtered}) => {
 
       <select value={ filter.inStock === undefined ? '' : filter.inStock } onChange={ search } name='inStock' data-type='boolean'>
         <option value=''>All</option>
+        {
+          Object.entries(stockFacet).map( entry => {
+            const key = entry[0];
+            const value = entry[1];
+            if(value.count === 0){
+              return null;
+            }
+            return (
+              <option key={ key } value={ value.value }>
+                { value.label } ({ value.count })
+              </option>
+            );
+          })
+        }
+        {
+          /*
         <option value='true'>In Stock</option>
         <option value='false'>Out of Stock</option>
+          */
+        }
       </select>
 
       <select value={filter.rating === undefined ? '' : filter.rating} onChange={ search } name='rating'>
         <option value=''>All</option>
+        {
+          Object.entries(ratingFacet).map( (entry)=> {
+            return (
+              <option key={ entry[0]} value={ entry[0] }>{ entry[0]} ({ entry[1]})</option>
+            );
+          })
+        }
+        {
+          /*
         <option>GREAT</option>
         <option>GOOD</option>
         <option>MEH</option>
+        */
+        }
       </select>
 
       <ul>
@@ -71,7 +99,29 @@ const mapState = (state, { match }) => {
   if(filter.inStock !== undefined){
     filtered = filtered.filter( product => product.inStock === filter.inStock);
   }
+
+  const ratingFacet = (filter.inStock === undefined ? state.products : state.products.filter(product => product.inStock === filter.inStock)).reduce((acc, product)=> {
+    const key = product.rating;
+    acc[key] = acc[key] || 0;
+    acc[key]++;
+    return acc;
+  }, {});
+
+  const stockFacet = (filter.rating ? state.products.filter(product=> product.rating === filter.rating) :state.products).reduce((acc, product)=> {
+    if(product.inStock){
+      acc.inStock.count++;
+    }
+    else {
+      acc.outOfStock.count++;
+    }
+    return acc;
+  }, {
+    inStock: { value: true, count: 0, label: 'In Stock'},
+    outOfStock: { value: false, count: 0, label: 'Out of Stock'},
+  });
   return {
+    stockFacet,
+    ratingFacet,
     products: state.products,
     filtered,
     filter
